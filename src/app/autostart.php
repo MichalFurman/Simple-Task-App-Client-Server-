f9478ef0e7319f608fd0724a24344c639f42e9ef
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-07-05 11:10:24 +0200

    Simple Tasks App Client Server on PHP and React JS

--- src/app/autostart.php
+++ src/app/autostart.php
@@ -1,7 +1,6 @@
 <?php   
     /** plik, w którym możemy sobie zdefiniować poszczególne usługi które mają zastartować zanim ruszą docelowe serwisy
      *  plik conf jest odczytywany do tablicy, która jest czytana globalnie,
-     *  jest to spowodowane tym, że nie każdy serwer PHP pozwala przypisać wartości zmiennym ENV
      *  konfigurację na apache można przypisać do zmiennych środowiskowych i w ten sposób z niej korzystać
      * 
      *  Dodatkowo mamy inicjowaną instancję singletone dla połączenia z bazą danych, tylko przy pierwszym razie wymaga ona by podać jej
