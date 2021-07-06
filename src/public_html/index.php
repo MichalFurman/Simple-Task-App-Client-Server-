687eaa1f8ef44b7f9c23cc2e1d6b3a969e0f6ee4
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-07-06 19:53:24 +0200

    Added exceptions for handling paths for required files.

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -4,7 +4,22 @@
      *  komenda:  PHP -S localhost:8001 w katalogu src/public_html
     */
     
-    require_once __DIR__ . '../../../vendor/autoload.php';
-    require_once __DIR__ . '../../../src/app/autostart.php';  
-    require_once __DIR__ . '../../../src/app/routes.php';
+    /* autoload.php */
+    if (file_exists(__DIR__ . '../../../vendor/autoload.php')) require __DIR__ . '../../../vendor/autoload.php';
+    else if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) require __DIR__ . '/../../../vendor/autoload.php';
+    else if (file_exists('../../../vendor/autoload.php')) require '../../../vendor/autoload.php';
+    else exit ('Can not load file "autoload.php" in: "src/public_html/index.php", plase check path and file.');
+
+    /* autostart.php */
+    if (file_exists(__DIR__ . '../../../src/app/autostart.php')) require __DIR__ . '../../../src/app/autostart.php';
+    else if (file_exists(__DIR__ . '/../../../src/app/autostart.php')) require __DIR__ . '/../../../src/app/autostart.php';
+    else if (file_exists('../../../src/app/autostart.php')) require '../../../src/app/autostart.php';
+    else exit ('Can not load file "autostart.php" in: "src/public_html/index.php", plase check path and file.');
+
+    /* routes.php */
+    if (file_exists(__DIR__ . '../../../src/app/routes.php')) require __DIR__ . '../../../src/app/routes.php';
+    else if (file_exists(__DIR__ . '/../../../src/app/routes.php')) require __DIR__ . '/../../../src/app/routes.php';
+    else if (file_exists('../../../src/app/routes.php')) require '../../../src/app/routes.php';
+    else exit ('Can not load file "routes.php" in: "src/public_html/index.php", plase check path and file.');
+
 ?>
