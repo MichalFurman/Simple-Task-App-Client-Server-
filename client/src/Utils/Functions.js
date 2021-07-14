155c3479f6f6d9ba1cef176ae054bbb080a25f54
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-07-14 17:37:28 +0200

    Update Functions.js
    Add semicolons.

--- client/src/Utils/Functions.js
+++ client/src/Utils/Functions.js
@@ -1,57 +1,54 @@
-import axios from 'axios'
-import {API} from "./config"
+import axios from 'axios';
+import {API} from "./config";
 
 export const itemsList = () => {
-  return axios
-  .get(API+'/', {
+  return axios.get(API+'/', {
       headers: {'Content-Type': 'application/json'}
     })
     .then(response => {
       if (response.status === 200 && response.data !== undefined && response.data.length > 0) {
-        return response.data
+        return response.data;
       }
-      return null
+      return null;
     })
     .catch((errors) => {
-      if (errors) console.log(errors)
-      alert('We have some troubles. Perhaps server is down.')
-      return null
+      if (errors) console.log(errors);
+      alert('We have some troubles. Perhaps server is down.');
+      return null;
     })
 
 }
 
 export const getItem = (id) => {
-  return axios
-  .get(API+'/'+id, {
+  return axios.get(API+'/'+id, {
       headers: {'Content-Type': 'application/json'}
     })
     .then(response => {
       if (response.status === 200) {
-        return response.data
+        return response.data;
       }
-      return null
+      return null;
     })
     .catch((errors) => {
-      if (errors) console.log(errors)
-      alert('We have some troubles. Perhaps server is down.')
-      return null
+      if (errors) console.log(errors);
+      alert('We have some troubles. Perhaps server is down.');
+      return null;
     })
 }
 
 export const deleteItem = (id) => {
-  return axios
-  .delete(API+'/'+id, {
+  return axios.delete(API+'/'+id, {
       headers: {'Content-Type': 'application/json'}
     })
     .then(response => {
       if (response.status === 200) {
-        return true
+        return true;
       }
-      return null
+      return null;
     })
     .catch((errors) => {
-      if (errors) console.log(errors)
-      alert('We have some troubles. Perhaps server is down.')
-      return null
+      if (errors) console.log(errors);
+      alert('We have some troubles. Perhaps server is down.');
+      return null;
     })
 }
