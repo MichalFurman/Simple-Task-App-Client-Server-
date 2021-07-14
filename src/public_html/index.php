ade2e2183ea7db16790c354981c5a63c3f0cff3b
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-07-14 17:32:50 +0200

    Update index.php

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -5,21 +5,12 @@
     */
     
     /* autoload.php */
-    if (file_exists(__DIR__ . '../../../vendor/autoload.php')) require __DIR__ . '../../../vendor/autoload.php';
-    else if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) require __DIR__ . '/../../../vendor/autoload.php';
-    else if (file_exists('../../../vendor/autoload.php')) require '../../../vendor/autoload.php';
-    else exit ('Can not load file "autoload.php" in: "src/public_html/index.php", plase check path and file.');
+    require '../../../vendor/autoload.php';
 
     /* autostart.php */
-    if (file_exists(__DIR__ . '../../../src/app/autostart.php')) require __DIR__ . '../../../src/app/autostart.php';
-    else if (file_exists(__DIR__ . '/../../../src/app/autostart.php')) require __DIR__ . '/../../../src/app/autostart.php';
-    else if (file_exists('../../../src/app/autostart.php')) require '../../../src/app/autostart.php';
-    else exit ('Can not load file "autostart.php" in: "src/public_html/index.php", plase check path and file.');
+    require '../../../src/app/autostart.php';
 
     /* routes.php */
-    if (file_exists(__DIR__ . '../../../src/app/routes.php')) require __DIR__ . '../../../src/app/routes.php';
-    else if (file_exists(__DIR__ . '/../../../src/app/routes.php')) require __DIR__ . '/../../../src/app/routes.php';
-    else if (file_exists('../../../src/app/routes.php')) require '../../../src/app/routes.php';
-    else exit ('Can not load file "routes.php" in: "src/public_html/index.php", plase check path and file.');
+    require '../../../src/app/routes.php';
 
 ?>
