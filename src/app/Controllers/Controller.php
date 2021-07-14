9ced6d4799ee06324e899e15089887c2af737c48
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-07-14 18:22:32 +0200

    Update Controller.php

--- src/app/Controllers/Controller.php
+++ src/app/Controllers/Controller.php
@@ -3,12 +3,10 @@
 
     class Controller
     {
-
-		public function notFound()
-      	{
-			http_response_code(404);
-			echo json_encode(array('data'=>'not found'));	
-	  	}   
-
+	public function notFound()
+      	    {
+		http_response_code(404);
+		echo json_encode(array('data'=>'not found'));	
+	    }   
     }
 ?>
