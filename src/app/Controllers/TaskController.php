fb85e80f1ec19964406a938485060b92d7cb954d
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-07-15 16:37:02 +0200

    Update TaskController.php

--- src/app/Controllers/TaskController.php
+++ src/app/Controllers/TaskController.php
@@ -2,53 +2,45 @@
     namespace Myvendor\Actaskman\Controllers;
 
     use Myvendor\Actaskman\Services\TaskService;
+    use Myvendor\Actaskman\Services\ResponseService;
 
     class TaskController
     {
-	use ControllersTrait;
 
-	private $taskService;
+		private $taskService;
+		private $responseService;
 
-	public function __construct() 
-	{
-		$this->taskService = new TaskService();
-	}
+		public function __construct() 
+		{
+			$this->taskService = new TaskService();
+			$this->responseService = new ResponseService();
+		}
+			
 
+		public function index()
+		{
+			$this->responseService->response($this->taskService->index());
+		}
 		
-	public function index()
-      	{
-		$result = $this->checkResult($this->taskService->index());
-		http_response_code($result['status']);
-		echo json_encode($result['data']);	
+		public function show($id)  
+		{
+			$this->responseService->response($this->taskService->show($id));
+		}
+
+		public function store()  
+		{
+			$this->responseService->response($this->taskService->store());
+		}
+
+		public function update($id)  
+		{
+			$this->responseService->response($this->taskService->update($id));
+		}
+
+		public function delete($id)  
+		{
+			$this->responseService->response($this->taskService->delete($id));
+		}
 	}
-      
-      	public function show($id)  
-      	{
-		$result = $this->checkResult($this->taskService->show($id));
-		http_response_code($result['status']);
-		echo json_encode($result['data']);	
-      	}
-
-	public function store()  
-      	{
-		$result = $this->checkResult($this->taskService->store());
-		http_response_code($result['status']);
-		echo json_encode($result['data']);	
-      	}
-
-	public function update($id)  
-      	{
-		$result = $this->checkResult($this->taskService->update($id));
-		http_response_code($result['status']);
-		echo json_encode($result['data']);	
-      	}
-
-	public function delete($id)  
-      	{
-		$result = $this->checkResult($this->taskService->delete($id));
-		http_response_code($result['status']);
-		echo json_encode($result['data']);	
-      	}
-    }
 
 ?>
